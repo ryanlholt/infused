51ae5b632f97c00058c26cc13e19d2c55d71ed52
Ryan Holt <ryanlholt.88@gmail.com>
2021-07-11 17:10:47 -0400

    Refactor token handling

--- src/Http/Middleware/CheckValidInfusionsoftToken.php
+++ src/Http/Middleware/CheckValidInfusionsoftToken.php
@@ -5,6 +5,7 @@ namespace RyanLHolt\Infused\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Infusionsoft\InfusionsoftException;
 use RyanLHolt\Infused\Models\InfusionsoftToken;
 
@@ -19,22 +20,28 @@ class CheckValidInfusionsoftToken
      */
     public function handle(Request $request, Closure $next)
     {
-        $userId = Auth::user()->id;
-        $token = InfusionsoftToken::where('user_id', $userId);
+        $ifs_token = $request->session()->get('infused_ifs_token', false);
 
-        if($token){
-            app('infusionsoft')->setToken($token);
+        if ($ifs_token) {
+            app('infused')->infusionsoft->setToken($ifs_token);
+        }
 
-            if($token->end_of_life < now()){
-                try {
-                    app('infusionsoft')->refreshAccessToken();
-                } catch(InfusionsoftException $e) {
-                    $e->getMessage();
-                    // @todo: Add in config var for infusionsoft authorization route and redirect there.
-                }
+        $stored_ifs_token = InfusionsoftToken::where('user_id', Auth::user()->id);
+
+        if ($stored_ifs_token !== $ifs_token || $ifs_token->end_of_life < now()) {
+            try {
+                app('infused')->infusionsoft->refreshAccessToken();
+            } catch (InfusionsoftException $e) {
+                $e->getMessage();
+                /**
+                 * @todo: Add in config var for infusionsoft authorization
+                 * route and redirect there.
+                 */
             }
-        } else {
-            // @todo: Add in config var for infusionsoft authorization route and redirect there.
+        }
+
+        if (!$ifs_token) {
+            Session::put('infused_ifs_token', $stored_ifs_token);
         }
 
         return $next($request);
