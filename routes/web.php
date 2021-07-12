50e41709eb539dcf4bc8c534e29485082b9bdb8c
Ryan Holt <ryanlholt.88@gmail.com>
2021-07-12 15:50:32 -0400

    Cleanup routes file

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use RyanLHolt\Infused\Http\Controllers\InfusionsoftController;
 
 Route::group(['middleware' => ['web']], function () {
-    // Route::get('/infusionsoft/callback', [InfusionsoftController::class, 'storeToken']);
     Route::get(config('infused.infusionsoft.redirectUri'), [InfusionsoftController::class, 'finishAuthorize']);
     Route::get(
         config('infused.infusionsoftSettings'),
