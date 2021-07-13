fafb20186001c469d65d4677d9872d843391502d
Ryan Holt <ryanlholt.88@gmail.com>
2021-07-12 22:13:42 -0400

    Add authorize link to settings view

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -4,5 +4,6 @@
     <div class="alert alert-block alert-success">
         <i class=" fa fa-check cool-green "></i>
         {{ nl2br(Session::get('infused_status')) }}
+        <a href="{{App::make(Infused::class)->infusionsoft->getAuthorizationUrl()}}">Authorize</a>
     </div>
 @endif
