43a4249fe3d8ea2cbaae64c6bff0198b377a46f4
Ryan Holt <ryanlholt.88@gmail.com>
2021-07-13 00:21:23 -0400

    Cleanup authorization view

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,9 +1,17 @@
 <h1>Infused Settings</h1>
 
-@if(Session::has('infused_status'))
+@if (session()->exists('infused_auth_status') && session('infused_auth_status') === 1)
+    <div class="alert alert-block alert-success flex items-center">
+        <p class="text-lg font-bold">Infusionsoft is Authorized!</p>
+        <i class=" fa fa-check cool-green inline"></i>
+        <a href="{{app('infused')->infusionsoft()->getAuthorizationUrl()}}">Re-Authorize</a>
+    </div>
+@else
     <div class="alert alert-block alert-success">
-        <i class=" fa fa-check cool-green "></i>
-        {{ nl2br(Session::get('infused_status')) }}
-        <a href="{{App::make(Infused::class)->infusionsoft->getAuthorizationUrl()}}">Authorize</a>
+        <p class="text-lg font-bold">Infusionsoft is not Authorized!</p>
+        <i class=" fa fa-check color-danger "></i>
+        <a href="{{app('infused')->infusionsoft()->getAuthorizationUrl()}}">Re-Authorize</a>
     </div>
 @endif
+
+
