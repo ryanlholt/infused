89a5ba9b20b65e3842fbf5db73fb7832d7d62971
Ryan Holt <ryanlholt.88@gmail.com>
2021-07-13 23:31:14 -0400

    Fixes for StyleCI

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use RyanLHolt\Infused\Http\Controllers\InfusionsoftController;
 
-Route::group(['middleware' => ['auth','web']], function () {
+Route::group(['middleware' => ['auth', 'web']], function () {
     Route::get(config('infused.infusionsoftAuthorize'), [InfusionsoftController::class, 'finishAuthorize']);
     Route::get(config('infused.infusionsoftSettings'), [InfusionsoftController::class, 'settings'])
               ->name('infused.infusionsoft.settings');
